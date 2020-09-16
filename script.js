a631e9f06d4c1b08a024d3aa1b6d53b7f0f49158
Veronica Jones <veronicajones2112@gmail.com>
2020-09-16 12:39:43 -0500

    Added all the cards and their correct values.

--- script.js
+++ script.js
@@ -19,25 +19,57 @@ init()
 /******************** * I think I could add the values of all 52 cards here... ***************/
 document.querySelector('.btn-draw').addEventListener('click', function() {
     if(gamePlaying) {
-        // Cycle through all cards. I'm only using clubs right now cuz I don't know how to do it otherwise
-        let card = Math.floor(Math.random() * 13) + 1;
-        let card2 = Math.floor(Math.random() * 13) + 1;
+        // Cycle through all cards.
+//        let card = Math.floor(Math.random() * 20) + 1;
+        let card2 = Math.floor(Math.random() * 52) + 1;
         // Display bottom card (higher up)
         let cardDOM = document.querySelector('.card');
         cardDOM.style.display = 'block';
         cardDOM.src = 'images/card-back.jpg';
             // Make value of Jack, Queen, and King = 10;
-            if (card === 11 || card === 12 | card === 13) {
-                card = 10;
-            }
+//            if (card === 11 || card === 12 | card === 13) {
+//                card = 10; 
+//            }
         // Display top card (lower down)
         let cardDOM2 = document.querySelector('.card2');
         cardDOM2.style.display = 'block';
         cardDOM2.src = 'images/card-' + card2 + '.svg';
-            // Make value of Jack, Queen, and King = 10;
-            if (card2 === 11 || card2 === 12 | card2 ===13) {
+            // Make value of Jacks, Queens, and Kings = 10;
+            if (card2 === 11 || card2 === 12 || card2 === 13 || card2 === 24 || card2 === 25 || card2 === 26 || card2 === 37 || card2 === 38 || card2 === 39 || card2 === 50 || card2 === 51 || card2 === 52) {
                 card2 = 10;
+            } else if (card2 === 14 || card2 === 27 || card2 === 40) { // Ace of Diamonds/Spades/Heart
+                card2 = 1;
+            } 
+            else if (card2 === 15 || card2 === 28 || card2 === 41) { // 2 of Diamonds/Spades/Heart
+                card2 = 2;
+            } 
+            else if (card2 === 16 || card2 === 29 || card2 === 42) { // 3 of Diamonds/Spades/Heart
+                card2 = 3;
+            }  
+            else if (card2 === 17 || card2 === 30 || card2 === 43) { // 4 of Diamonds/Spades/Heart
+                card2 = 4;
+            } 
+            else if (card2 === 18 || card2 === 31 || card2 === 44) { // 5 of Diamonds/Spades/Heart
+                card2 = 5;
+            } 
+            else if (card2 === 19 || card2 === 32 || card2 === 45) { // 6 of Diamonds/Spades/Heart
+                card2 = 6;
+            } 
+            else if (card2 === 20 || card2 === 33 || card2 === 46) { // 7 of Diamonds/Spades/Heart
+                card2 = 7;
             }
+            else if (card2 === 21 || card2 === 34 || card2 === 47) { // 8 of Diamonds/Spades/Heart
+                card2 = 8;
+            } 
+            else if (card2 === 22 || card2 === 35 || card2 === 48) { // 9 of Diamonds/Spades/Heart
+                card2 = 9;
+            } 
+            else if (card2 === 23 || card2 === 36 || card2 === 49) { // 10 of Diamonds/Spades/Heart
+                card2 = 10;
+            } 
+     
+        
+        
         // Get the score
         roundScore += card2;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
